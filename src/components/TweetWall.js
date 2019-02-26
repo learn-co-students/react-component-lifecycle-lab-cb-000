65363e9dbe1ea06126f2d1512252606e12a415de
cdcardle <cdcardle@gmail.com>
2019-02-26 11:16:50 -0800

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,21 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() {
+    this.setState({
+      tweets: this.state.tweets.concat(this.props.newTweets)
+    });
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (nextProps.newTweets === this.state.tweets)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: nextProps.newTweets.concat(this.state.tweets)
+    })
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
