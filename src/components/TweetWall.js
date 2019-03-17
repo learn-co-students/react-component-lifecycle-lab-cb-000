161db17b8caa65ef00e20f7227ea24b3cd61a4bb
Felice Forby <forby.1@gmail.com>
2019-03-18 08:40:33 +0900

    implement Tweets stream functionality

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,24 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
+  // Get the new tweets passed down from App and update state before mounting
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    });
+  }
+  
   // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  shouldComponentUpdate(nextProps) {
+    return nextProps.newTweets.length > 0;
+  }
+  
+  // Add new tweets to the beginning of the tweets array before updating
+  componentWillReceiveProps(nextProps) {
+    const tweets = [...nextProps.newTweets.slice(), ...this.state.tweets.slice()];
+    
+    this.setState({tweets});
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
